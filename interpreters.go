437a7fe370454464bf2814cba7ea1077aadc6551
Alessandro Arzilli <alessandro.arzilli@gmail.com>
2011-02-02 16:27:10 +0100

    interpreting bytes/words

--- interpreters.go
+++ interpreters.go
@@ -4,6 +4,7 @@ import (
 	"regexp"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type Interpreter struct {
@@ -17,6 +18,7 @@ var interpreters []Interpreter = []Interpreter{
 	Interpreter{ "HTML decimal character reference", IntHTMLDec },
 	Interpreter{ "HTML hexadecimal character reference", IntHTMLHex },
 	Interpreter{ "HTML entity", IntHTMLEntity },
+	Interpreter{ "Bytes", IntBytes },
 }
 
 func IntCharacter(arg string) (bool, int, []byte) {
@@ -62,7 +64,7 @@ func IntHTMLHex(arg string) (bool, int, []byte) {
 	return interpretCodepoint(arg[2:len(arg)-1], true)
 }
 
-var HTMLEntityRE *regexp.Regexp = regexp.MustCompile("^&[a-zA-Z]+;$")
+var HTMLEntityRE *regexp.Regexp = regexp.MustCompile("^&[a-fA-F]+;$")
 
 func IntHTMLEntity(arg string) (bool, int, []byte) {
 	if !HTMLEntityRE.MatchString(arg) { return false, -1, nil }
@@ -71,3 +73,34 @@ func IntHTMLEntity(arg string) (bool, int, []byte) {
 	if !ok { return false, -1, nil }
 	return true, codepoint, nil
 }
+
+var BytesRE *regexp.Regexp = regexp.MustCompile("^[0-9a-fA-F ]+$")
+
+func IntBytes(arg string) (bool, int, []byte) {
+	if !BytesRE.MatchString(arg) { return false, -1, nil }
+
+	r := make([]byte, 0)
+	
+	for _, segment := range strings.Split(arg, " ", -1) {
+		var n byte
+		
+		switch len(segment) {
+		case 2:
+			_, err := fmt.Sscanf(segment, "%x", &n)
+			if err != nil { return false, -1, nil }
+			r = append(r, n)
+		case 4:
+			_, err := fmt.Sscanf(segment[0:2], "%x", &n)
+			if err != nil { return false, -1, nil }
+			r = append(r, n)
+			_, err = fmt.Sscanf(segment[2:], "%x", &n)
+			if err != nil { return false, -1, nil }
+			r = append(r, n)
+		default:
+			return false, -1, nil
+		} 
+	}
+	
+	return true, -1, r
+}
+
