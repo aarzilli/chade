259733d013de916d53520765a2e71fdfca92266f
Alessandro Arzilli <alessandro.arzilli@gmail.com>
2011-02-19 18:35:50 +0100

    attempt to implement debugger for shift_jis

--- decoders.go
+++ decoders.go
@@ -24,18 +24,21 @@ var decoders []Decoder = []Decoder{
 	Decoder{ "Windows-1253 (greek windows)", MakeDecIconv("windows-1253") },
 	Decoder{ "ISO-8859-8 (hebrew)", MakeDecIconv("iso-8859-8") },
 	Decoder{ "Windows-1255", MakeDecIconv("windows-1255") },
+	Decoder{ "KOI8-R", MakeDecIconv("koi8-r") },
+	Decoder{ "Windows-1251 (russian windows)", MakeDecIconv("windows-1251") },
+	Decoder{ "Windows-874 (thai windows)", MakeDecIconv("windows-874") },
+	Decoder{ "ISO-8859-11 (thai)", MakeDecIconv("iso-8859-11") },
+	Decoder{ "TIS-620 (thai)", MakeDecIconv("tis-620") },
+	Decoder{ "Windows-1258 (vietnamese)", MakeDecIconv("windows-1258") },
+
+	Decoder{ "Shift-JIS", ShiftJISDecoder },
+	
 
 	// Shift-JIS
-	// ISO-2022-JP
+	// ISO-2022-JS
 	// EUC-JP
 	// EUC-KR
 	// ISO-2022-KR
-	// KOI8-R (cyrillic)
-	// Windows-1251 (russian windows)
-	// Windows-874 (thai windows)
-	// ISO-8859-11 (thai)
-	// TIS-620 (thai)
-	// Windows-1258 (vietnamese)
 	// EUC-CN (chinese)
 	// BIG5 (chinese)
 	// GBK (chinese)
@@ -49,13 +52,17 @@ func DecASCII(in []byte) (bool, int, string) {
 	return true, int(in[0]), ""
 }
 
+func IconvDecoder(in []byte, charset string) (bool, int, string) {
+	out, err := iconv.Conv("UTF-8", charset, string(in))
+	if err != nil { return false, -1, "Rejected by iconv" }
+	if len(out) == 0 { return false, -1, "Rejected by iconv" }
+	return true, []int(out)[0], ""
+}
+
 func MakeDecIconv(charset string) func([]byte) (bool, int, string) {
 	return func(in []byte) (bool, int, string) {
 		if len(in) != 1 { return false, -1, "More than one character" }
-		out, err := iconv.Conv("UTF-8", charset, string(in))
-		if err != nil { return false, -1, "Rejected by iconv" }
-		if len(out) == 0 { return false, -1, "Rejected by iconv" }
-		return true, []int(out)[0], ""
+		return IconvDecoder(in, charset)
 	}
 }
 
@@ -146,4 +153,95 @@ func DecUtf16Common(ints []uint16) (bool, int, string) {
 	result := (uint32(hisur) << 10) + uint32(lowsur) + 0x10000
 
 	return true, int(result), ""
+}
+
+const (
+	SINGLE_BYTE int = iota
+	FORBIDDEN_FIRST_BYTE
+	FIRST_BYTE
+	NONSTANDARD_FIRST_BYTE
+)
+
+const (
+	FORBIDDEN_SECOND_BYTE int = iota
+	SECOND_BYTE_EVEN
+	SECOND_BYTE_ODD
+)
+
+func ClassifyShiftJISByte1(in byte) int {
+	switch in & 0xF0 {
+	case 0x00, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70: return SINGLE_BYTE
+	case 0x80:
+		if in == 0x80 { return FORBIDDEN_FIRST_BYTE }
+		return FIRST_BYTE
+	case 0x90: return FIRST_BYTE
+	case 0xA0:
+		if in == 0xA0 { return FORBIDDEN_FIRST_BYTE }
+		return FIRST_BYTE
+	case 0xB0, 0xC0, 0xD0: return SINGLE_BYTE
+	case 0xE0: return FIRST_BYTE
+	case 0xF0:
+		if (in >= 0xF3) && (in <= 0xF9) { return NONSTANDARD_FIRST_BYTE }
+		return FORBIDDEN_FIRST_BYTE
+	}
+
+	return -1
+}
+
+func ClassifyShiftJISByte2(in byte) int {
+	switch in & 0xF0 {
+	case 0x00, 0x10, 0x20, 0x30: return FORBIDDEN_SECOND_BYTE
+	case 0x40, 0x50, 0x60, 0x70, 0x80, 0x90:
+		if in == 0x7F { return FORBIDDEN_SECOND_BYTE }
+		if in == 0x9F { return SECOND_BYTE_EVEN }
+		return SECOND_BYTE_ODD
+	case 0xA0, 0xB0, 0xC0, 0xD0, 0xF0:
+		if in <= 0xFD {
+			return SECOND_BYTE_EVEN }
+		return FORBIDDEN_SECOND_BYTE
+	}
+
+	return FORBIDDEN_SECOND_BYTE
+}
+
+
+
+func ShiftJISCheckByte2(in []byte) string {
+	if len(in) > 2 { return "Too many bytes (never more than 2 bytes in a Shift-JIS character)" }
+	switch ClassifyShiftJISByte2(in[1]) {
+	case FORBIDDEN_SECOND_BYTE:
+		return "Unacceptable second byte (and first byte was also non-standard";
+	case SECOND_BYTE_ODD:
+		if in[0] & 0x01 == 0x00 {
+			return "Mismatch between second byte and first byte (second byte requires an odd first byte)"
+		}
+	case SECOND_BYTE_EVEN:
+		if in[0] & 0x01 != 0x00 {
+			return "Mismatch between second byte and first byte (second byte requires an even first byte)"
+		}
+	}
+
+	return ""
+}
+
+
+func ShiftJISDecoder(in []byte) (bool, int, string) {
+	switch ClassifyShiftJISByte1(in[0]) {
+	case SINGLE_BYTE:
+		if len(in) > 1 { return false, -1, "Too many bytes, the first byte indicates only one is needed" }
+	case FORBIDDEN_FIRST_BYTE:
+		return false, -1, "First (only?) byte is forbidden in Shift-JIS"
+	case FIRST_BYTE:
+		if r := ShiftJISCheckByte2(in); r != "" {
+			return false, -1, r
+		}
+		return IconvDecoder(in, "shift_jis")
+	case NONSTANDARD_FIRST_BYTE:
+		if r := ShiftJISCheckByte2(in); r != "" {
+			return false, -1, r
+		}
+		return false, -1, "Non-standard first byte used (but everything else is ok, so this could be an emoji)"
+	}
+
+	return false, -1, "This shouldn't happen"
 }
